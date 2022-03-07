537627c07f9782dcb8d4736c91d712f6173d049c
MCHSL <michal.sul+githubalt@wp.pl>
2022-03-07 19:58:52 +0100

    fix CORS not allowing API calls

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -53,9 +53,11 @@ INSTALLED_APPS = [
 	'wklejki',
 	"graphene_django",
     "django_filters",
+	'corsheaders',
 ]
 
 MIDDLEWARE = [
+	'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -150,3 +152,8 @@ GRAPHENE = {
 		"graphql_jwt.middleware.JSONWebTokenMiddleware",
 	],
 }
+
+
+CORS_ALLOWED_ORIGINS = [
+	'http://localhost:8000',
+]

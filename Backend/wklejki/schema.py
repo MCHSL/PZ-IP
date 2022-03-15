c4eff8694286023fe2bcb89efb575d8373bb04eb
MCHSL <michal.sul+githubalt@wp.pl>
2022-03-15 12:51:44 +0100

    complete user CRUDment

--- Backend/wklejki/schema.py
+++ Backend/wklejki/schema.py
@@ -2,7 +2,7 @@ from graphene_django import DjangoObjectType
 import graphene
 from django.contrib.auth import get_user_model
 from django.contrib.auth.base_user import BaseUserManager
-from graphql_jwt.decorators import staff_member_required, login_required
+from graphql_jwt.decorators import staff_member_required, login_required, superuser_required
 
 class UserType(DjangoObjectType):
 	class Meta:
@@ -38,20 +38,43 @@ class CreateUser(graphene.Mutation):
 
 		return CreateUser(user=user)
 
+class UpdateUser(graphene.Mutation):
+	user = graphene.Field(UserType)
+
+	class Arguments:
+		id = graphene.Int(required=True, description="ID of the user to update")
+		username = graphene.String(description="New username (optional)")
+		email = graphene.String(description="New email (optional)")
+		is_staff = graphene.Boolean(description="New staff status (optional)")
+
+	@superuser_required
+	def mutate(self, info, username=None, email=None, is_staff=None):
+		user = get_user_model().objects.get(pk=id)
+		if username is not None:
+			user.username = username
+		if email is not None:
+			user.email = email
+		if is_staff is not None:
+			user.is_staff = is_staff
+		user.save()
+
+		return UpdateUser(user=user)
+
 class DeleteUser(graphene.Mutation):
 	ok = graphene.Boolean()
 
 	class Arguments:
-		email = graphene.String(required=True)
+		id = graphene.Int(required=True, description="ID of the user to delete")
 
 	@staff_member_required
-	def mutate(self, info, email):
-		user = get_user_model().objects.get(email=email)
+	def mutate(self, info, id):
+		user = get_user_model().objects.get(pk=id)
 		user.delete()
 		return DeleteUser(ok=True)
 
 class Mutation(graphene.ObjectType):
 	create_user = CreateUser.Field()
+	update_user = UpdateUser.Field()
 	delete_user = DeleteUser.Field()
 
 

5846761a8fce51e53dc02053cab5a8f3943f88bf
macieksulikowski <suleymaniana99@gmail.com>
2022-03-23 11:36:23 +0100

    [T][12] Simple test

--- Backend/wklejki/tests.py
+++ Backend/wklejki/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from graphene_django.utils.testing import GraphQLTestCase
+
+class MyFancyTestCase(GraphQLTestCase):
+    def test_registration(self):
+        response = self.query(
+            '''
+            mutation {
+                createUser(username: "test_user", email: "test@test.com", password: "123") {
+                    user {
+                        id
+                    }
+                }
+            }
+            '''
+        )
+
+        content = json.loads(response.content)
+
+        # This validates the status code and if you get errors
+        self.assertResponseNoErrors(response)
+    
+    def test_login(self):
+        response = self.query(
+            '''
+            mutation {
+                tokenAuth( email: "test@test.com", password: "123") {
+                    token
+                }
+            }
+            '''
+        )
+
+        content = json.loads(response.content)
+
+        # This validates the status code and if you get errors
+        self.assertResponseNoErrors(response)

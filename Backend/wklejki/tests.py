6233f3c4732cdcd0f599fb81133f35a4953fdcca
MCHSL <michal.sul+githubalt@wp.pl>
2022-03-23 19:26:42 +0100

    fix up tests a bit

--- Backend/wklejki/tests.py
+++ Backend/wklejki/tests.py
@@ -2,10 +2,10 @@ import json
 
 from graphene_django.utils.testing import GraphQLTestCase
 
-class MyFancyTestCase(GraphQLTestCase):
-    def test_registration(self):
-        response = self.query(
-            '''
+
+class AuthenticationTests(GraphQLTestCase):
+	def test_registration_and_login(self):
+		response = self.query('''
             mutation {
                 createUser(username: "test_user", email: "test@test.com", password: "123") {
                     user {
@@ -13,26 +13,30 @@ class MyFancyTestCase(GraphQLTestCase):
                     }
                 }
             }
-            '''
-        )
+            ''')
 
-        content = json.loads(response.content)
+		print("")
+		print(response)
+		print(response.content)
 
-        # This validates the status code and if you get errors
-        self.assertResponseNoErrors(response)
-    
-    def test_login(self):
-        response = self.query(
-            '''
+		content = json.loads(response.content)
+
+		# This validates the status code and if you get errors
+		self.assertResponseNoErrors(response)
+
+		response = self.query('''
             mutation {
                 tokenAuth( email: "test@test.com", password: "123") {
                     token
                 }
             }
-            '''
-        )
+            ''')
 
-        content = json.loads(response.content)
+		print("")
+		print(response)
+		print(response.content)
 
-        # This validates the status code and if you get errors
-        self.assertResponseNoErrors(response)
+		content = json.loads(response.content)
+
+		# This validates the status code and if you get errors
+		self.assertResponseNoErrors(response)

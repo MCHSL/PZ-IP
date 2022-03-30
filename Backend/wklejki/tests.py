d7d07985ec2e61de8fe23f400f0a74a38a7cd407
macieksulikowski <suleymaniana99@gmail.com>
2022-03-30 13:19:02 +0200

    [T][26] Test user CRUD

--- Backend/wklejki/tests.py
+++ Backend/wklejki/tests.py
@@ -3,6 +3,8 @@ import json
 
 # 3rd-Party
 from graphene_django.utils.testing import GraphQLTestCase
+from django.contrib.auth import get_user_model
+from graphql_jwt.shortcuts import get_token
 
 
 class AuthenticationTests(GraphQLTestCase):
@@ -19,13 +21,8 @@ class AuthenticationTests(GraphQLTestCase):
             '''  # noqa: E501
         )
 
-        print("")
-        print(response)
-        print(response.content)
-
         json.loads(response.content)
 
-        # This validates the status code and if you get errors
         self.assertResponseNoErrors(response)
 
         response = self.query(
@@ -38,11 +35,248 @@ class AuthenticationTests(GraphQLTestCase):
             '''
         )
 
-        print("")
-        print(response)
-        print(response.content)
-
         json.loads(response.content)
 
-        # This validates the status code and if you get errors
         self.assertResponseNoErrors(response)
+
+
+class UserCrudTest(GraphQLTestCase):
+    def setUp(self):
+        self.admin = get_user_model().objects.create(
+            email="admin@ad.min",
+            username="admin",
+            password="admin1",
+            is_superuser=True,
+            is_staff=True,
+        )
+        self.token = get_token(self.admin)
+
+    def test_user_crud(self) -> None:
+        create_result = self.query(
+            """
+                mutation ($email: String!, $password: String!, $username: String!) {
+                    createUser(email: $email, password: $password, username: $username) {
+                        user {
+                            id
+                        }
+                    }
+                }
+            """,
+            variables={
+                "email": "someuser@gmail.com",
+                "password": "123",
+                "username": "someuser",
+            },
+            headers={"HTTP_AUTHORIZATION": f"JWT {self.token}"},
+        )
+        self.assertResponseNoErrors(create_result)
+
+        result = json.loads(create_result.content)
+        user_id = int(result["data"]["createUser"]["user"]["id"])
+
+        read_result = self.query(
+            """
+                query ($id: Int!) {
+                    user(id: $id) {
+                        id
+                        email
+                        username
+                    }
+                }
+            """,
+            variables={"id": user_id},
+            headers={"HTTP_AUTHORIZATION": f"JWT {self.token}"},
+        )
+        self.assertResponseNoErrors(read_result)
+
+        result = json.loads(read_result.content)
+        self.assertEqual(result["data"]["user"]["id"], user_id)
+        self.assertEqual(result["data"]["user"]["email"], "someuser@gmail.com")
+        self.assertEqual(result["data"]["user"]["username"], "someuser")
+
+        update_result = self.query(
+            """
+                mutation ($id: Int!, $email: String!, $username: String!) {
+                    updateUser(id: $id, email: $email, username: $username) {
+                        user {
+                            id
+                            email
+                            username
+                        }
+                    }
+                }
+            """,
+            variables={
+                "id": user_id,
+                "email": "anotheruser@gmail.com",
+                "username": "anotheruser",
+            },
+            headers={"HTTP_AUTHORIZATION": f"JWT {self.token}"},
+        )
+        self.assertResponseNoErrors(update_result)
+
+        result = json.loads(update_result.content)
+        self.assertEqual(result["data"]["updateUser"]["user"]["id"], user_id)
+        self.assertEqual(
+            result["data"]["updateUser"]["user"]["email"], "anotheruser@gmail.com"
+        )
+        self.assertEqual(
+            result["data"]["updateUser"]["user"]["username"], "anotheruser"
+        )
+
+        delete_result = self.query(
+            """
+                mutation ($id: Int!) {
+                    deleteUser(id: $id) {
+                        ok
+                    }
+                }
+            """,
+            variables={"id": user_id},
+            headers={"HTTP_AUTHORIZATION": f"JWT {self.token}"},
+        )
+        self.assertResponseNoErrors(delete_result)
+
+        result = json.loads(delete_result.content)
+        self.assertEqual(result["data"]["deleteUser"]["ok"], True)
+
+        verify_delete = self.query(
+            """
+                query ($id: Int!) {
+                    user(id: $id) {
+                        id
+                    }
+                }
+            """,
+            variables={"id": user_id},
+            headers={"HTTP_AUTHORIZATION": f"JWT {self.token}"},
+        )
+        self.assertResponseHasErrors(verify_delete)
+
+
+class PasteCrudTest(GraphQLTestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create(
+            email="fake@email.com",
+            username="test",
+            password="AAAAAAA",
+        )
+        self.token = get_token(self.user)
+
+    def test_paste_crud(self) -> None:
+        create_result = self.query(
+            """
+                mutation ($title: String!, $content: String!, $isPrivate: Boolean!) {
+                    createPaste(title: $title, content: $content, private: $isPrivate) {
+                        paste {
+                            id
+                        }
+                    }
+                }
+            """,
+            variables={
+                "title": "test title",
+                "content": "test content",
+                "isPrivate": False,
+            },
+            headers={"HTTP_AUTHORIZATION": f"JWT {self.token}"},
+        )
+        self.assertResponseNoErrors(create_result)
+
+        result = json.loads(create_result.content)
+        paste_id = int(result["data"]["createPaste"]["paste"]["id"])
+
+        read_result = self.query(
+            """
+                query ($id: Int!) {
+                    paste(id: $id) {
+                        id
+                        title
+                        content
+                        private
+                        author {
+                            id
+                            username
+                        }
+                    }
+                }
+            """,
+            variables={"id": paste_id},
+            headers={"HTTP_AUTHORIZATION": f"JWT {self.token}"},
+        )
+        self.assertResponseNoErrors(read_result)
+
+        result = json.loads(read_result.content)
+        self.assertEqual(result["data"]["paste"]["id"], paste_id)
+        self.assertEqual(result["data"]["paste"]["title"], "test title")
+        self.assertEqual(result["data"]["paste"]["content"], "test content")
+        self.assertEqual(result["data"]["paste"]["private"], False)
+        self.assertEqual(result["data"]["paste"]["author"]["id"], self.user.id)
+        self.assertEqual(
+            result["data"]["paste"]["author"]["username"], self.user.username
+        )
+
+        update_result = self.query(
+            """
+                mutation ($id: Int!, $title: String!, $content: String!, $private: Boolean!) {
+                    updatePaste(id: $id, title: $title, content: $content, private: $private) {
+                        paste {
+                            id
+                            title
+                            content
+                            private
+                        }
+                    }
+                }
+            """,
+            variables={
+                "id": paste_id,
+                "title": "updated title",
+                "content": "updated content",
+                "private": True,
+            },
+            headers={"HTTP_AUTHORIZATION": f"JWT {self.token}"},
+        )
+        self.assertResponseNoErrors(update_result)
+
+        result = json.loads(update_result.content)
+        self.assertEqual(result["data"]["updatePaste"]["paste"]["id"], paste_id)
+        self.assertEqual(result["data"]["updatePaste"]["paste"]["private"], True)
+        self.assertEqual(
+            result["data"]["updatePaste"]["paste"]["title"], "updated title"
+        )
+        self.assertEqual(
+            result["data"]["updatePaste"]["paste"]["content"], "updated content"
+        )
+
+        delete_result = self.query(
+            """
+                mutation ($id: Int!) {
+                    deletePaste(id: $id) {
+                        ok
+                    }
+                }
+            """,
+            variables={"id": paste_id},
+            headers={"HTTP_AUTHORIZATION": f"JWT {self.token}"},
+        )
+        self.assertResponseNoErrors(delete_result)
+
+        result = json.loads(delete_result.content)
+        self.assertEqual(result["data"]["deletePaste"]["ok"], True)
+
+        verify_delete = self.query(
+            """
+                query ($id: Int!) {
+                    paste(id: $id) {
+                        id
+                    }
+                }
+            """,
+            variables={"id": paste_id},
+            headers={"HTTP_AUTHORIZATION": f"JWT {self.token}"},
+        )
+        self.assertResponseHasErrors(verify_delete)
+
+
+# docker compose build && docker compose run --rm --entrypoint="python manage.py test" backend
